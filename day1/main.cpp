bd733f940ecf0a90e2ca58bd58683d8002c49100
Samuel Erdtman <erdtman@spotify.com>
2018-12-02 23:20:27 +0100

    minor changes and my input

--- day1/main.cpp
+++ day1/main.cpp
@@ -3,63 +3,55 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
-#include <set>
-//#include <curl/curl.h>
+#include <map>
 
 using namespace std;
 
 
 int main(int argc, const char * argv[]) {
-	cout << "Start" << '\n';
-	string file = "C:\\Users\\elias\\code\\adventofcode2019\\day1_input_1_E.txt";
-	//string input = "-1\n-2\n-3";
-	string nr = "";
-	string::size_type sz;
-	//istringstream iss(input);
-	ifstream infile(file);
+	if (argc < 2) {
+		cout << "Usage:" << endl;
+		cout << "day1 <input>" << endl;
+ 		return 1;
+	}
+	
+	// Common data storage
 	vector<int> v_input;
-	set<int> s_visited_f;
-	int twice = 0;
-	bool found = false;
-	s_visited_f.insert(twice);
-	int frekvens = 0;
 
-	// add input in to a vector to be able to loop through it all
-	while (infile >> nr) {
-		v_input.push_back(stoi(nr, &sz));
-	}
-	vector<int>::const_iterator itv;
-
-	while (!found) {
-		itv = v_input.begin();
-		while (itv != v_input.end()) {
-			frekvens += *itv;
-			if (s_visited_f.count(frekvens)) {
-				twice = frekvens;
-				found = true;
-			}
-			else {
-				s_visited_f.insert(frekvens);
-			}
-			itv++;
-			if (found) break;
+	{ // Read input
+		cout << "Start with input file '" << argv[1] << "'" <<std::endl;
+		ifstream infile(argv[1]);
+		string nr = "";
+		string::size_type sz;
+		while (infile >> nr) {
+			v_input.push_back(stoi(nr, &sz));
 		}
-		cout << " Frekvens: " << frekvens << " Twice: " << twice << endl;
 	}
 
-	cout << "twice: " << twice << endl;
-	/*
-	int i = 0;
-	int total_f = 0;
-	int in_f = 0;
-	while (infile >> nr) {
-		in_f = stoi(nr, &sz);
-		total_f += in_f;
+	{ // Solve part one
+		int current = 0;
+		vector<int>::const_iterator itv = v_input.begin();
+		while (itv != v_input.end()) {
+			current += *itv;
+			itv++;
+		}
+		std::cout << "Answer part one : " << current << endl;
+	}
 
-		cout << i << " total_f: " << total_f << endl;
-		++i;
-
-	}*/
+	{ // Solve part two
+		int current = 0;
+		std::map<int, bool> visited;
+		vector<int>::const_iterator itv = v_input.begin();
+		while (!visited[current]) {
+			visited[current] = true;
+			current += *itv;
+			itv++;
+			if(itv == v_input.end()) {
+				itv = v_input.begin();
+			}
+		}
+		std::cout << "Answer part two : " << current << endl;
+	}
 
     return 0;
 }
